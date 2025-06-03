73e02df5fd7d34af5d21fe0dfd4dd30185e65a02
abiranlopezkms <abiranlopez@kms-technology.com>
2025-06-03 10:04:51 -0700

    Adds conditional test case execution in CommonHooks
    Introduces a `shouldSkip` method to check for excluded test cases,
    preventing unnecessary browser setup and teardown for specified
    test cases. Enhances efficiency and avoids redundant actions.
    
    Excludes 'Test Cases/OCR' as an example in the skip list.

--- Test Listeners/CommonHooks.groovy
+++ Test Listeners/CommonHooks.groovy
@@ -1,11 +1,14 @@
 import com.kms.katalon.core.annotation.BeforeTestCase
 import com.kms.katalon.core.annotation.AfterTestCase
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.context.TestCaseContext
 
 class CommonHooks {
 
     @BeforeTestCase
-    def setupBrowser() {
+    def setupBrowser(TestCaseContext testCaseContext) {
+        if (shouldSkip(testCaseContext)) return
+
         WebUI.openBrowser('')
         WebUI.navigateToUrl('https://datatables.net/examples/data_sources/ajax')
         WebUI.waitForPageLoad(10)
@@ -13,8 +16,17 @@ class CommonHooks {
     }
 
     @AfterTestCase
-    def teardownBrowser() {
+    def teardownBrowser(TestCaseContext testCaseContext) {
+        if (shouldSkip(testCaseContext)) return
+
         WebUI.closeBrowser()
+    }
+
+    private boolean shouldSkip(TestCaseContext ctx) {
+        def excluded = [
+            'Test Cases/OCR'
+        ]
+        return excluded.contains(ctx.getTestCaseId())
     }
 }
 
